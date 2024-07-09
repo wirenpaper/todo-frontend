01cec86466f0a25785b50cd19577dfd45015e74b
wnp <chumchumchira@gmail.com>
2024-07-09 10:49:49 +0500

    modified shopping list to todo list

---
+++ app/item.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ItemProps {
+  text: string;
+  onDelete: () => void;
+}
+
+const Item: React.FC<ItemProps> = ({ text, onDelete }) => {
+  return (
+    <div className="flex w-48 justify-between">
+      <p>{text}</p>
+      <button onClick={onDelete} className="text-red-600 font-bold size-4">
+        x
+      </button>
+    </div>
+  );
+};
+
+export default Item;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      width: {
+        '25rem': '25rem',
+        '45': '12rem',
+      },
+      backgroundColor: {
+        'buttonColor': '#E5E5E5',
+      },
+      fontSize: {
+        'custom-sm': '0.85rem'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
